b77db565285a71b47ea867d281e18727ddc3a1b6
Isaac Aires <playerredbrasil@gmail.com>
2025-04-09 17:03:34 -0300

    Initialize AMPMap project structure with essential files and configurations

---
+++ ampmap/ampmap/src/main/java/com/ampmap/ampmap/AmpmapApplication.java
@@ -0,0 +1,13 @@
+package com.ampmap.ampmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmpmapApplication.class, args);
+	}
+
+}

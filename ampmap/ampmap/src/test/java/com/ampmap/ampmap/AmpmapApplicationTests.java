53e47831c96c0a917a97b444887d9d0c079be2cb
Isaac Aires <playerredbrasil@gmail.com>
2025-04-09 16:41:00 -0300

    setup do backend

---
+++ ampmap/ampmap/src/test/java/com/ampmap/ampmap/AmpmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ampmap.ampmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmpmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

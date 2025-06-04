98886948dbd0f0d0f970059429e998a0102af611
caioo <matossousacaio.cms@gmail.com>
2025-06-04 14:23:09 -0300

    Getters/setters

--- ampmap/ampmap/src/main/java/com/ampmap/ampmap/model/entities/Feedback.java
+++ ampmap/ampmap/src/main/java/com/ampmap/ampmap/model/entities/Feedback.java
@@ -1,19 +1,33 @@
 package com.ampmap.ampmap.model.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Feedback {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    @NotBlank
+    @Size(max = 500)
     private String comentario;
 
+    @Min(1)
+    @Max(10)
     private Integer nota;
 
     @ManyToOne

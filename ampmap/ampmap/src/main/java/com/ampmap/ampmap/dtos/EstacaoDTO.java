67ace869843eff3d332e4ba1c9160bff51ee93f9
caioo <matossousacaio.cms@gmail.com>
2025-06-11 16:13:09 -0300

    Tava sem os feedbacks, corrigido, agora esta retornando certo

--- ampmap/ampmap/src/main/java/com/ampmap/ampmap/dtos/EstacaoDTO.java
+++ ampmap/ampmap/src/main/java/com/ampmap/ampmap/dtos/EstacaoDTO.java
@@ -1,6 +1,8 @@
 package com.ampmap.ampmap.dtos;
 
 import com.ampmap.ampmap.enumn.EstacaoStatus;
+import com.ampmap.ampmap.model.entities.Feedback;
+
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
@@ -12,6 +14,7 @@ public record EstacaoDTO(
         String conector,
         Double potencia,
         BigDecimal valorPorHora, // Adicionado
+        List<Feedback> feedbacks,
         EstacaoStatus status,
         List<String> fotosUrls // Adicionado
 
